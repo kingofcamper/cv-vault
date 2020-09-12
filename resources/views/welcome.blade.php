79410b71e63c4f559c27b133f19217a10eb2da3f
bilel <bilel.belghith123@gmail.com>
2020-09-12 03:20:36 +0200

    add home page design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,33 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+  <title>Letter - Simple Sign Up Form</title>
+<!-- 
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+Letter Template 
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
+https://templatemo.com/tm-510-letter
+
+
+
+Template Re-distribution is NOT allowed.
+
+-->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">  <!-- Google web font "Open Sans" -->
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  
+  <link rel="stylesheet" href="css/demo.css" />
+  <link rel="stylesheet" href="css/templatemo-style.css">
+  <style>   
+.full-height {
                 height: 100vh;
-                margin: 0;
             }
-            .full-height {
-                height: 100vh;
-            }
+
             .flex-center {
                 align-items: center;
                 display: flex;
@@ -41,51 +47,63 @@
             .title {
                 font-size: 84px;
             }
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+</style>    
+  
+  <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+        
     </head>
+
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/cvs') }}">Mes CV</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <div id="particles-js"></div>
+        
+            <ul class="cb-slideshow">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            <div class="container-fluid">
+                <div class="row cb-slideshow-text-container ">
+                    <div class= "tm-content col-xl-6 col-sm-8 col-xs-8 ml-auto section ">
+                    <P class="mb-5">Add a Cv library containing experience, formation, competence and project for recruiters to facilitate their job</P>
+                    </div>
+                    <header class="mb-5"><h3>
+                        <div class="tm-content flex-center position-ref full-height " style="" >
+                        @if (Route::has('login'))
+                                <div class="top-right">
+                                    @auth
+                                        <a href="{{ url('/cvs') }}">Mes CV</a>
+                                    @else
+                                        <a href="{{ route('login') }}">Login</a> &nbsp;&nbsp;
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                                    @if (Route::has('register'))
+                                               &nbsp; <a href="{{ route('register') }}">Register</a>
+                                    @endif
+                                    @endauth
+                                </div>
+                            @endif
+                        </div>
+                    </h3></header>
+                
+                    
+                    
+                            
+                    
+                    
+                    </div>
+                </div>  
+                <div class="footer-link">
+                    <p>Copyright © 2020 Recruitement App 
+                    
+                    - Design: Belghith Bilel</p>
                 </div>
-            </div>
-        </div>
+            </div>  
     </body>
+
+    <script type="text/javascript" src="js/particles.js"></script>
+    <script type="text/javascript" src="js/aapp.js"></script>
 </html>
